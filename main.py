3fab17697fe50a67d7bc35a9b3b947972b39711a
ebenezum <ebenezum@o2.pl>
2019-11-03 22:15:19 +0100

    adding readAllProjects method

--- main.py
+++ main.py
@@ -2,18 +2,34 @@
 
 from jira import JIRA
 import pandas as pd
-import numpy as np
+import json
+
+JIRA_USER = 'marcin.wroblewski@scalac.io'
+JIRA_PASS = 'k1T27hk7HhuYlM2tmOIf6AFB'
+SERVER_URL = 'http://prp.atlassian.net'
 
 
-def downloadTimeLogs(serverURL, projectName):
+def readAllProjects():
     jira_options = {
-            'server':serverURL
-            }
-    jira_user = 'marcin.wroblewski@scalac.io'
-    jira_pass = 'k1T27hk7HhuYlM2tmOIf6AFB'
-    jac = JIRA(options=jira_options, basic_auth=(jira_user, jira_pass))
+        'server': SERVER_URL
+    }
 
-    block_size =100
+    jac = JIRA(options=jira_options, basic_auth=(JIRA_USER, JIRA_PASS))
+    projNames = []
+    projects = jac.projects()
+    for proj in projects:
+        projNames.append(proj.name)
+    return json.dumps(projNames)
+
+
+def downloadTimeLogs(projectName):
+    jira_options = {
+        'server': SERVER_URL
+    }
+
+    jac = JIRA(options=jira_options, basic_auth=(JIRA_USER, JIRA_PASS))
+
+    block_size = 100
     block_num = 0
     auth = list()
     wlDt = list()
@@ -23,11 +39,11 @@ def downloadTimeLogs(serverURL, projectName):
 
     while True:
         start_idx = block_num * block_size
-        issues_in_project = jac.search_issues('project='+projectName, start_idx, block_size)
+        issues_in_project = jac.search_issues(
+            'project='+projectName, start_idx, block_size)
         if len(issues_in_project) == 0:
             break
         block_num += 1
-
 
         for issue in issues_in_project:
             wrkl = jac.worklogs(issue.key)
@@ -38,19 +54,24 @@ def downloadTimeLogs(serverURL, projectName):
                 wlTS.append(wrk.timeSpentSeconds/3600)
                 wlDt.append(wrk.started[:10])
     preFrame = {
-        'Summary':summ,
-        'Key':iKey,
-        'Author':auth,
-        'TimeSpent':wlTS,
-        'Date':wlDt
+        'Summary': summ,
+        'Key': iKey,
+        'Author': auth,
+        'TimeSpent': wlTS,
+        'Date': wlDt
     }
     result = pd.DataFrame(preFrame)
     # pivot = pd.pivot_table(result, values='TimeSpent',index=['Date','Summary'], columns='Author',aggfunc=np.sum)
-    with pd.ExcelWriter('final/'+ projectName + '.xlsx') as writer:
+    with pd.ExcelWriter('final/' + projectName + '.xlsx') as writer:
         result.to_excel(writer, sheet_name='Raw Data')
         # pivot.to_excel(writer, sheet_name='Pivot')
     return "Done"
 
+
+# print(
+#     downloadTimeLogs(serverURL='http://prp.atlassian.net', projectName='PRP')
+# )
+
 print(
-    downloadTimeLogs(serverURL='http://prp.atlassian.net', projectName='PRP')
+    readAllProjects()
     )
