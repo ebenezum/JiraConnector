119df31371cb73466ec05913aa87668709a2f20d
ebenezum <ebenezum@o2.pl>
2020-04-16 00:41:04 +0200

    Update main.py
    improving code - fixing method that produced duplicates
    downloading all worklogs for project
    for further filtering

--- main.py
+++ main.py
@@ -38,65 +38,63 @@ def readAllProjects():
     return json.dumps(projNames)
 
 
-def downloadTimeLogs(projectName, dateOffset):
+def downloadTimeLogs(projectName):
+    # connecting to jira server
     jira_options = {
         'server': SERVER_URL
     }
-    startDate = estimateStartDate(dateOffset)
     jac = JIRA(options=jira_options, basic_auth=(JIRA_USER, JIRA_PASS))
     block_size = 100
     block_num = 0
-    auth = list()
-    wlDt = list()
-    wlTS = list()
-    summ = list()
-    iKey = list()
-    pKey = list()
-    pDesc = list()
-
     while True:
         start_idx = block_num * block_size
         issues_in_project = jac.search_issues(
             'project='+projectName, start_idx, block_size)
         if len(issues_in_project) == 0:
             break
+        else:
+            # iterating through issues to get all timelogs
+            for issue in issues_in_project:
+                auth = list()
+                wlDt = list()
+                wlTS = list()
+                summ = list()
+                iKey = list()
+                pKey = list()
+                pDesc = list()
+                wrkl = jac.worklogs(issue.key)
+                if len(wrkl) > 0:
+                    for wrk in wrkl:
+                        try:
+                            pKey.append(issue.fields.parent.key)
+                            pDesc.append(issue.fields.parent.fields.summary)
+                        except AttributeError:
+                            pKey.append(issue.key)
+                            pDesc.append(issue.fields.summary)
+                        finally:
+                            summ.append(issue.fields.summary)
+                            iKey.append(issue.key)
+                            auth.append(wrk.author)
+                            wlTS.append(wrk.timeSpentSeconds/3600)
+                            wlDt.append(wrk.started[:10])
+                    preFrame = {
+                        'Parent Key': pKey,
+                        'Parent Description': pDesc,
+                        'Key': iKey,
+                        'Summary': summ,
+                        'Author': auth,
+                        'TimeSpent': wlTS,
+                        'Date': wlDt
+                    }
+                    result = pd.DataFrame(preFrame)
+                    result.to_csv(
+                        'final/' + projectName + '.csv',
+                        mode='a',
+                        header=False,
+                        sep='|')
+        print(block_num)
         block_num += 1
 
-        for issue in issues_in_project:
-            wrkl = jac.worklogs(issue.key)
-            # print(
-            #     "Parent: " + issue.fields.parent.key +
-            #     " Child: " + issue.key
-            #     )
-            for wrk in wrkl:
-                if strToDate(wrk.created) >= startDate:
-                    try:
-                        pKey.append(issue.fields.parent.key)
-                        pDesc.append(issue.fields.parent.fields.summary)
-                    except AttributeError:
-                        pKey.append(issue.key)
-                        pDesc.append(issue.fields.summary)
-                    finally:
-                        summ.append(issue.fields.summary)
-                        iKey.append(issue.key)
-                        auth.append(wrk.author)
-                        wlTS.append(wrk.timeSpentSeconds/3600)
-                        wlDt.append(wrk.started[:10])
-            preFrame = {
-                'Summary': summ,
-                'Key': iKey,
-                'Parent Key': pKey,
-                'Parent Description': pDesc,
-                'Author': auth,
-                'TimeSpent': wlTS,
-                'Date': wlDt
-            }
-            result = pd.DataFrame(preFrame)
-            result.to_csv(
-                'final/' + projectName + '.csv',
-                mode='a',
-                header=False,
-                sep='|')
     print("Done")
 
 
@@ -119,6 +117,10 @@ def listNextSprintIssues(sprintName):
 
 
 if __name__ == "__main__":
+    print(datetime.datetime.now())
+    dtStart = datetime.datetime.now()
     # listNextSprintIssues('"BP Sprint 46"')
     # readAllProjects()
-    downloadTimeLogs("BP", 10)
+    downloadTimeLogs("BP")
+    print(datetime.datetime.now())
+    print(datetime.datetime.now() - dtStart)
